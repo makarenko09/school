695a554ca537e8de8e923ae651a4efc6b3c56bcd
makarenko009 <konmakarenko@outlook.com>
2025-04-12 15:31:57 +0300

    some changes - set dependencies

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.service.StudentService;
 
+import java.util.Collection;
+
 @RestController
 @RequestMapping("/student")
 public class StudentController {
@@ -13,6 +15,10 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    @GetMapping("/{amount}")
+    public Collection<Student> getStudentsWithAge(@PathVariable int amount) {
+        return studentService.getStudentsWithValueAge(amount);
+    }
     @GetMapping("/{id}")
     public Student getStudent(@PathVariable long id) {
         return studentService.getStudent(id);
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -3,8 +3,11 @@ package ru.hogwarts.school.service;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 @Service
 public class StudentService {
     private final Map<Long, Student> studentsMap = new HashMap<>();
@@ -27,5 +30,15 @@ public class StudentService {
 
     public Student deleteStudent(long id) {
         return studentsMap.remove(id);
+    }
+
+    public Collection<Student> getAllStudents() {
+        return studentsMap.values();
+    }
+
+    public Collection<Student> getStudentsWithValueAge(int age) {
+        return studentsMap.values().stream()
+                .filter(student -> student.getAge() == age)
+                .collect(Collectors.toList());
     }
 }
