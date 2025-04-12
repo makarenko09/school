871c319313c81995728e0e95127a0fbef325ff47
makarenko009 <konmakarenko@outlook.com>
2025-04-12 11:32:01 +0300

    average changes - step two (create Student-, Faculty-class)

---
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -0,0 +1,41 @@
+package ru.hogwarts.school.model;
+
+public class Student {
+    private Long id;
+    private String name;
+    private int age;
+
+    public Student(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student newObject(Long id, String name, int age) {
+        return new Student(id, name, age);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
