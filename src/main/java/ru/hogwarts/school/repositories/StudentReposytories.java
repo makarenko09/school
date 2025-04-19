0ba502ebdfd114abe9cc0c764421fcf174e9f3a0
makarenko009 <konmakarenko@outlook.com>
2025-04-19 09:38:49 +0300

    Revert "average changes - set annotations"
    This reverts commit f71b1690fbc1744b5f88fd3d18df5cbbb42a1d8d.

--- src/main/java/ru/hogwarts/school/repositories/StudentReposytories.java
+++
@@ -1,8 +0,0 @@
-package ru.hogwarts.school.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.hogwarts.school.model.Student;
-
-public interface StudentReposytories extends JpaRepository<Student, Long> {
-
-}
