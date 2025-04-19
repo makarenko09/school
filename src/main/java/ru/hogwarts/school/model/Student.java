6767fd3fcbdbcbaaa5b2e7d742a03bd063b17b87
makarenko009 <konmakarenko@outlook.com>
2025-04-19 13:32:23 +0300

    average changes - step 3

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -1,54 +1,67 @@
 package ru.hogwarts.school.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
 import java.util.Objects;
 
-public class Student {
-    private Long id;
-    private String name;
-    private int age;
+    @Entity
+    public class Student {
+        @Id
+        @GeneratedValue
+        private Long id;
 
-    public Student(Long id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
+        private String name;
+        private int age;
 
-    public Student newObject(Long id, String name, int age) {
-        return new Student(id, name, age);
-    }
+        public Student(Long id, String name, int age) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
 
-    public Long getId() {
-        return id;
-    }
+        public Student() {
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+        }
 
-    public String getName() {
-        return name;
-    }
+        public Student newObject(Long id, String name, int age) {
+            return new Student(id, name, age);
+        }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+        public Long getId() {
+            return id;
+        }
 
-    public int getAge() {
-        return age;
-    }
+        public void setId(Long id) {
+            this.id = id;
+        }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+        public String getName() {
+            return name;
+        }
 
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Student student)) return false;
-        return age == student.age && Objects.equals(id, student.id) && Objects.equals(name, student.name);
-    }
+        public void setName(String name) {
+            this.name = name;
+        }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Student student)) return false;
+            return age == student.age && Objects.equals(id, student.id) && Objects.equals(name, student.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, age);
+        }
     }
-}
