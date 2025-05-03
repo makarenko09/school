e83f3dff3032f8cd9008a8ea43072691c9740b9d
makarenko009 <konmakarenko@outlook.com>
2025-05-03 14:13:30 +0300

    average changes - first and second steps - writing data to local disk and DB

---
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -0,0 +1,43 @@
+package ru.hogwarts.school.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Avatar {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String filePath;
+    private long fileSize;
+    private String mediaType;
+    @Lob
+    private byte[] data;
+    @OneToOne
+    private Student student;
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
