98dd2594b150eb653c1f8b377236da2098b03d76
makarenko009 <konmakarenko@outlook.com>
2025-06-06 00:25:37 +0300

    average changes - added JPA repository's methods with @Query: getFiveLateStudentsById, getAverageAgeOfStudentsByAge, getCountOfStudentsByName

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -2,13 +2,14 @@ package ru.hogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Collection<Student> findByAgeBetween(int min, int max);
 
