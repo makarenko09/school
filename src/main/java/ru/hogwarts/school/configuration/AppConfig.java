5fca911ed5cd66828c67a4f54841cde289468cba
makarenko009 <konmakarenko@outlook.com>
2025-06-13 15:57:24 +0300

    final changes - refactor and () -> prettyCode'sString, and collapse

--- src/main/java/ru/hogwarts/school/configuration/AppConfig.java
+++ src/main/java/ru/hogwarts/school/configuration/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AppConfig implements PortProvider {
-@Value("${server.port}")
+    @Value("${server.port}")
     private Integer port;
 
     @Override
