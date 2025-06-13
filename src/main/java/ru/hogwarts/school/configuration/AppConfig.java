2a6537ede4264b7fc920fd4993683b50374ed38f
makarenko009 <konmakarenko@outlook.com>
2025-06-13 13:59:54 +0300

    some changes - write second step - successful getMainPort from application.properties

--- src/main/java/ru/hogwarts/school/configuration/AppConfig.java
+++ src/main/java/ru/hogwarts/school/configuration/AppConfig.java
@@ -1,14 +1,14 @@
-package ru.hogwarts.school.service;
+package ru.hogwarts.school.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AppConfig {
+public class AppConfig implements PortProvider {
 @Value("${server.port}")
     private Integer port;
 
+    @Override
     public Integer getPort() {
         return port;
     }
--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,21 +1,22 @@
 package ru.hogwarts.school.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.hogwarts.school.service.AppConfig;
+import ru.hogwarts.school.configuration.PortProvider;
 
-@RestController("/port")
+@RestController
+@RequestMapping("/port")
 public class InfoController {
 
-    private final AppConfig appConfig;
+    private final PortProvider portProvider;
 
-    public InfoController(AppConfig appConfig) {
-        this.appConfig = appConfig;
+    public InfoController(PortProvider portProvider) {
+        this.portProvider = portProvider;
     }
 
     @GetMapping
     public String info() {
-        return "Server port: " + appConfig.getPort();
+        return "Server port: " + portProvider.getPort();
     }
-
 }
