33befd1cfe25dc4313f541c0028da5d0abdfd070
makarenko09 <163112091+makarenko09@users.noreply.github.com>
2025-06-15 14:08:22 +0300

    Merge pull request #1
    * average changes - step two (create Student-, Faculty-class)
    
    * average changes - step three (create CRUD on services)
    
    * average changes - step four (create CRUD on services)
    
    * some changes - set dependencies
    
    * some changes
    
    * average changes - add getFacultiesWithColor Method and other
    
    * average changes - add getFacultiesWithColor Method and other
    
    * average changes - set annotations
    
    * average changes - step 4
    
    * Revert "average changes - step 4"
    
    * Revert "average changes - set annotations"
    
    * final changes
    
    * final changes
    
    * final changes

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,40 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Collection;
+@RestController
+@RequestMapping("/faculty")
+public class FacultyController {
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+    @GetMapping("/someFaculties/{color}")
+    public Collection<Faculty> getFacultiesWitColor(@PathVariable String color) {
+        return facultyService.getFacultiesWithValueColor(color);
+    }
+    @GetMapping("/{id}")
+    public Faculty getFaculty(@PathVariable long id) {
+        return facultyService.getFaculty(id);
+    }
+
+    @PostMapping
+    public Faculty addFaculty(@RequestBody Faculty faculty) {
+        return facultyService.createFaculty(faculty);
+    }
+
+    @PutMapping
+    public Faculty updateFaculty(@RequestBody Faculty faculty) {
+        return facultyService.updateFaculty(faculty);
+    }
+
+    @DeleteMapping("/{id}")
+    public Faculty deleteFaculty(@PathVariable long id) {
+        return facultyService.deleteFaculty(id);
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,54 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+public class StudentService {
+    private final Map<Long, Student> studentsMap = new HashMap<>();
+    private long lastId = 0;
+
+    public Student createStudent(Student student) {
+        student.setId(++lastId);
+        studentsMap.put((this.lastId), student);
+        return student;
+    }
+
+    public Student getStudent(long id) {
+        if (studentsMap.get(id) != null) {
+        return studentsMap.get(id);
+        } else {
+            throw new NoSuchSomeObjectException(" - " + id + " does not exist");
+        }
+    }
+
+    public Student updateStudent(Student student) {
+        Student someObj = studentsMap.values().stream().filter(obj -> Objects.equals(obj.getId(), student.getId())).map(obj -> obj.newObject(student.getId(), student.getName(), student.getAge())).findFirst().orElseThrow(() -> new NoSuchSomeObjectException(" - " + student + " does not exist"));
+        studentsMap.put(student.getId(), someObj);
+        return studentsMap.get(student.getId());
+    }
+
+    public Student deleteStudent(long id) {
+        if (studentsMap.get(id) != null) {
+            return studentsMap.remove(id);
+        } else {
+            throw new NoSuchSomeObjectException(" - " + id + " does not exist");
+        }
+    }
+
+    public Collection<Student> getAllStudents() {
+        return studentsMap.values();
+    }
+
+    public Collection<Student> getStudentsWithValueAge(int age) {
+        return studentsMap.values().stream()
+                .filter(obj -> obj.getAge() == age)
+                .collect(Collectors.toList());
+    }
+}
